6f103948b002456674d418a161590fab36a8e49d
Lisa Williams <williams.lisarenee@gmail.com>
2017-01-22 14:13:04 -0500

    heroku deploy

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Son of a Bun');
+INSERT INTO burgers (burger_name) VALUES ('Double Whatta-Burger with Cheese');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Buffalo Soldier', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger in Paradise ', true);
